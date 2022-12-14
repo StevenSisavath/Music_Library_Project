7f42dd2ceeec12d030abcc760a19cbf510892e01
Steven Sisavath <stevenlsisavath@gmail.com>
2022-12-14 06:55:55 -0600

    Added App.css + Added CSS to NavBar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MusicTable from "./Components/MusicTable/MusicTable";
 import CreateNewSong from "./Components/CreateNewSong/CreateNewSong";
 import SearchBar from "./Components/SearchBar/SearchBar";
 import NavBar from "./Components/NavigationBar/NavigationBar";
+import './App.css';
 
 function App() {
   const [songs, setSongs] = useState([]);
@@ -27,17 +28,25 @@ function App() {
   };
 
   function search(searchTerm){
-    console.log(songs.filter(song => song.title.includes(searchTerm) || song.artist.includes(searchTerm) || song.album.includes(searchTerm) || song.release_date.includes(searchTerm) || song.genre.includes(searchTerm)))
+    console.log(songs.filter(song => song.title.toLowerCase().includes(searchTerm) || song.artist.includes(searchTerm) || song.album.includes(searchTerm) || song.release_date.includes(searchTerm) || song.genre.includes(searchTerm)))
     const filtered = (songs.filter(song => song.title.toLowerCase().includes(searchTerm) || song.artist.includes(searchTerm) || song.album.includes(searchTerm) || song.release_date.includes(searchTerm) || song.genre.includes(searchTerm)))
     setSongs(filtered)
   };
 
   return (
-    <div>
-      <NavBar/>
-      <MusicTable parentSongs = {songs}/>
-      <CreateNewSong addNewSongProperty = {addNewSong}/>
-      <SearchBar search={search}/>
+    <div className="nav_font">
+      <div>
+        <NavBar/>
+      </div>
+      <div className="nav_font">
+        <MusicTable parentSongs = {songs}/>
+      </div>
+      <div>
+        <CreateNewSong addNewSongProperty = {addNewSong}/>
+      </div>
+      <div>
+        <SearchBar search={search}/>
+      </div>
     </div>
   );
 }
